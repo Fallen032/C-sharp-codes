7f37381002c2c369c4c267d097402a230f922e3d
FireLord <94450839+Fallen032@users.noreply.github.com>
2024-02-19 06:54:47 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.Write("Enter Plate number: ");
+            String cartype = Console.ReadLine();
+
+            switch (cartype) {
+                case "1":
+                    Console.Write("Enter Plate number: ");
+                    String platenum = Console.ReadLine();
+                    break;
+            }
+            
+        }
+    }
+}
