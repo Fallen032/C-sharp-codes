aa8dcc6841a14cb57c846456433ce78bf0261d21
FireLord <94450839+Fallen032@users.noreply.github.com>
2024-02-25 06:54:27 -0800

    Add files via upload

---
+++ PalindromeCheck.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace PalindromeCheck
+{
+    internal class Program
+    {
+        private static bool IsPalindrome(string wordOrPhrase)
+        {
+            var min = 0;
+            var max = wordOrPhrase.Length - 1;
+
+            while (min < max)
+            {
+                while (min < max && !char.IsLetterOrDigit(wordOrPhrase[min]))
+                {
+                    min++;
+                }
+
+                while (min < max && !char.IsLetterOrDigit(wordOrPhrase[max]))
+                {
+                    max--;
+                }
+
+                if (char.ToLower(wordOrPhrase[min]) != char.ToLower(wordOrPhrase[max]))
+                {
+                    return false;
+                }
+
+                min++;
+                max--;
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            char input;
+
+            do
+            {
+                Console.Write("Enter a word or phrase: ");
+                var wordOrPhrase = Console.ReadLine();
+
+                if (IsPalindrome(wordOrPhrase))
+                {
+                    Console.WriteLine("The phrase or word you entered is a palindrome!");
+                }
+                else
+                {
+                    Console.WriteLine("The phrase or word you entered is not a palindrome!");
+                }
+
+                Console.WriteLine("Do you want to try again? Press [y] to continue and [n] for no");
+                input = Convert.ToChar(Console.ReadLine());
+
+            } while (char.ToLower(input) == 'y');
+        }
+    }
+}
