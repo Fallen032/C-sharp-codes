b1055d55be64493f173b84d7a42fb1ff2b9de5f7
FireLord <94450839+Fallen032@users.noreply.github.com>
2024-02-25 03:51:12 -0800

    Add files via upload

---
+++ reverseString.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ReverseString
+{
+    internal class reverseString
+    {
+        public static string Reverse(string input)
+        {
+            // Converting string to character array 
+            char[] charArray = input.ToCharArray();
+            // Declaring an empty string
+            string reverseString = String.Empty;
+
+            int length = charArray.Length - 1;
+            int index = length;
+
+            while (index >= 0)
+            {
+                reverseString = reverseString + charArray[index];
+                index--;
+            }
+
+            return reverseString;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string userInput = Console.ReadLine();
+
+            string reverseInput = Reverse(userInput);
+
+            // Displaying both the original input and the reversed string
+            Console.WriteLine("Original String: {0}", userInput);
+            Console.WriteLine("Reversed String: {0}", reverseInput);
+
+            Console.ReadLine();
+        }
+    }
+}
