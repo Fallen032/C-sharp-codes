6933025a41ed40482d855b8245a8b48bee61d7a4
FireLord <94450839+Fallen032@users.noreply.github.com>
2024-02-25 05:14:48 -0800

    Add files via upload

---
+++ Factorial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, factor = 1;
+
+            Console.Write("Enter Num: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num <= 1)
+            {
+                Console.WriteLine("{0}! = {1}", num, factor);
+            }
+            else
+            {
+                for (int count = num; count >= 2; count--)
+                {
+                    factor = factor * count;
+                }
+                Console.WriteLine("{0}! = {1}", num, factor);
+            }
+            Console.ReadLine();
+        }
+    }
+}
