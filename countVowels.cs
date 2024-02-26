c5c1c5c0b21976ab054488f1c1fdf7d2dbf43a4d
FireLord <94450839+Fallen032@users.noreply.github.com>
2024-02-26 00:40:53 -0800

    Add files via upload

---
+++ countVowels.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+
+namespace countVowel
+{
+    internal class Program
+    {
+        static int CountVowels(string input)
+        {
+           
+            int count = 0;
+
+            foreach (char character in input.ToLower())
+            {
+                if ("aeiou".Contains(character))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            String  input = Console.ReadLine();
+
+            int vowels = CountVowels(input);
+            Console.WriteLine("number of vowels: {0}", vowels);
+            Console.ReadLine();
+        }
+    }
+}
