1898a25e7c700ea265cb1122cec6e2a95636b12e
FireLord <94450839+Fallen032@users.noreply.github.com>
2024-02-26 07:15:17 -0800

    Add files via upload

---
+++ calculatePow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class calculatePow
+    {
+        static int GetPow(int baseNum, int powNum)
+        {
+            int result = 1;
+
+            for (int i = 0; i < powNum; i++)
+            {
+                result *= baseNum;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the base number: ");
+            int baseNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the exponent: ");
+            int exponent = Convert.ToInt32(Console.ReadLine());
+
+            int result = GetPow(baseNumber, exponent);
+
+            Console.WriteLine($"{baseNumber}^{exponent} = {result}");
+            Console.ReadLine();
+        }
+    }
+}
