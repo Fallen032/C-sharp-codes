bc19d5f4b2e2f4c991a9215c0bb06bb20df4be3d
FireLord <94450839+Fallen032@users.noreply.github.com>
2024-02-25 23:26:30 -0800

    Add files via upload

---
+++ fizzBuzz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace fizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                String result = "";
+
+                if (i % 3 == 0)
+                {
+                    result = "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    result += "Buzz";
+                }
+                if (result.Length == 0)
+                {
+                    result = i.ToString();
+                }
+
+                Console.WriteLine(result);
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
